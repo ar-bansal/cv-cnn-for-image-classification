c2e7910815cadd035f40840a1e7a47f5dbbfe113
Aryan Bansal <aryanbansal1710@gmail.com>
2024-12-31 17:46:05 -0600

    Add train/validation split

--- train1.py
+++ train1.py
@@ -1,7 +1,8 @@
 import os
 import mlflow
+from sklearn.model_selection import train_test_split
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
 from ml.models import VGGStyleV1
@@ -13,9 +14,11 @@ if __name__ == "__main__":
     tracking_server_uri = get_tracking_uri()
     mlflow.set_tracking_uri(tracking_server_uri)
 
+    RANDOM_STATE = 0
     BATCH_SIZE = 64
     NUM_EPOCHS = 100
     NUM_WORKERS = os.cpu_count()
+    PIN_MEMORY = True if torch.cuda.is_available() else False
 
     transform = transforms.Compose([
         transforms.ToTensor(), 
@@ -25,7 +28,19 @@ if __name__ == "__main__":
         )
     ])
 
-    train_dataset = CIFAR10(root="./data/train", train=True, transform=transform, download=True)
+    dataset = CIFAR10(root="./data/train", train=True, transform=transform, download=True)
+    targets = dataset.targets
+
+    # Split the dataset into train and validation sets. 
+    train_indices, val_indices = train_test_split(
+        range(len(targets)), 
+        test_size=0.2, 
+        stratify=targets, 
+        random_state=RANDOM_STATE
+    )
+
+    train_dataset = Subset(dataset, train_indices)
+    val_dataset = Subset(dataset, val_indices)
     test_dataset = CIFAR10(root="./data/test", train=False, transform=transform, download=True)
 
     train_loader = DataLoader(
@@ -33,20 +48,27 @@ if __name__ == "__main__":
         batch_size=BATCH_SIZE, 
         shuffle=True, 
         num_workers=NUM_WORKERS, 
-        pin_memory=True if torch.cuda.is_available() else False
+        pin_memory=PIN_MEMORY
 
+    )
+    val_loader = DataLoader(
+        val_dataset, 
+        batch_size=BATCH_SIZE, 
+        shuffle=False, 
+        num_workers=NUM_WORKERS, 
+        pin_memory=PIN_MEMORY
     )
     test_loader = DataLoader(
         test_dataset, 
         batch_size=BATCH_SIZE, 
         shuffle=False,  
         num_workers=NUM_WORKERS, 
-        pin_memory=True if torch.cuda.is_available() else False
+        pin_memory=PIN_MEMORY
     )
 
     class_labels = [
-        "airplane", "automobile", "bird", "cat", 
-        "deer", "dog", "frog", "horse", "ship", "truck"
+        "airplane", "automobile", "bird", "cat", "deer", 
+        "dog", "frog", "horse", "ship", "truck"
     ]
    
     run_pipeline(
