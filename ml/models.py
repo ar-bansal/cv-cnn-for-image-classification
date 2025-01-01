8796e5fa93cb1ac13cc62d373242bc27ab738532
Aryan Bansal <aryanbansal1710@gmail.com>
2025-01-01 15:22:51 -0600

    refactor into VGGBase, which is inherited by the VGGStyleVx classes

--- ml/models.py
+++ ml/models.py
@@ -5,7 +5,61 @@ from torchmetrics import Accuracy
 import pytorch_lightning as L
 
 
-class VGGStyleV1(L.LightningModule):
+class VGGBase(L.LightningModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.accuracy_fn = Accuracy("multiclass", num_classes=10)
+
+    
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        outputs = self(x)
+
+        train_loss = self.loss_fn(outputs, y)
+        train_accuracy = self.accuracy_fn(outputs, y)
+
+        self.log("train_loss", train_loss, on_epoch=True)
+        self.log("train_accuracy", train_accuracy, on_epoch=True)
+
+        return train_loss
+    
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        outputs = self(x)
+
+        val_loss = self.loss_fn(outputs, y)
+        val_accuracy = self.accuracy_fn(outputs, y)
+
+        self.log("val_loss", val_loss, on_epoch=True)
+        self.log("val_accuracy", val_accuracy, on_epoch=True)
+    
+
+    def test_step(self, batch, batch_idx):
+        images, true_labels = batch
+        logits = self(images)
+
+        pred_labels = torch.argmax(logits, dim=1)
+
+        test_accuracy = self.accuracy_fn(pred_labels, true_labels)
+
+        self.log("test_accuracy", test_accuracy, on_epoch=True)
+        return test_accuracy
+    
+
+    def predict_step(self, batch, batch_idx):
+        logits = self(batch)
+
+        pred_labels = torch.argmax(logits, dim=1)
+        return pred_labels
+    
+
+    def configure_optimizers(self):
+        return optim.AdamW(self.parameters(), lr=1e-3)
+
+
+class VGGStyleV1(VGGBase):
     """
     Uses 1 VGG block.
     """
@@ -22,8 +76,6 @@ class VGGStyleV1(L.LightningModule):
         # in features = image size * number of filters
         self.fc = nn.Linear(16 * 16 * 32, 10)
 
-        self.loss_fn = nn.CrossEntropyLoss()
-        self.accuracy = Accuracy("multiclass", num_classes=10)
 
     def forward(self, x):
         x = self.block(x)
@@ -31,32 +83,8 @@ class VGGStyleV1(L.LightningModule):
         x = x.flatten(1)
         return self.fc(x)
     
-    def training_step(self, batch, batch_idx):
-        # return super().training_step(*args, **kwargs)
-        x, y = batch
-        outputs = self(x)
 
-        loss = self.loss_fn(outputs, y)
-        train_accuracy = self.accuracy(outputs, y)
-
-        self.log("train_loss", loss, on_epoch=True)
-        self.log("train_accuracy", train_accuracy, on_epoch=True)
-
-        return loss
-    
-    # def test_step(self, batch, batch_idx):
-    #     x, y = batch
-    #     logits = self(x)
-
-    #     y_preds = torch.argmax(logits)
-
-    #     return y_preds.cpu().numpy(), y.cpu().numpy()
-    
-    def configure_optimizers(self):
-        return optim.AdamW(self.parameters(), lr=1e-3)
-
-
-class VGGStyleV2(L.LightningModule):
+class VGGStyleV2(VGGBase):
     """
     Uses 2 VGG blocks. 
     """
@@ -81,8 +109,6 @@ class VGGStyleV2(L.LightningModule):
         # in features = image size * number of filters
         self.fc = nn.Linear(8 * 8 * 64, 10)
 
-        self.loss_fn = nn.CrossEntropyLoss()
-        self.accuracy = Accuracy("multiclass", num_classes=10)
 
     def forward(self, x):
         x = self.block1(x)
@@ -90,34 +116,9 @@ class VGGStyleV2(L.LightningModule):
         # Flatten the processed features
         x = x.flatten(1)
         return self.fc(x)
-    
-    def training_step(self, batch, batch_idx):
-        # return super().training_step(*args, **kwargs)
-        x, y = batch
-        outputs = self(x)
-
-        loss = self.loss_fn(outputs, y)
-        train_accuracy = self.accuracy(outputs, y)
-
-        self.log("train_loss", loss, on_epoch=True)
-        self.log("train_accuracy", train_accuracy, on_epoch=True)
-
-        return loss
-    
-    # def test_step(self, batch, batch_idx):
-    #     x, y = batch
-    #     logits = self(x)
-
-    #     y_preds = torch.argmax(logits)
-
-    #     return y_preds.cpu().numpy(), y.cpu().numpy()
-
-    def configure_optimizers(self):
-        return optim.AdamW(self.parameters(), lr=1e-3)
 
 
-
-class VGGStyleV3(L.LightningModule):
+class VGGStyleV3(VGGBase):
     """
     Uses 3 VGG blocks. 
     """
@@ -150,9 +151,6 @@ class VGGStyleV3(L.LightningModule):
         # in features = image size * number of filters
         self.fc = nn.Linear(4 * 4 * 128, 10)
 
-        self.loss_fn = nn.CrossEntropyLoss()
-        self.accuracy = Accuracy("multiclass", num_classes=10)
-
     def forward(self, x):
         x = self.block1(x)
         x = self.block2(x)
@@ -160,27 +158,3 @@ class VGGStyleV3(L.LightningModule):
         # Flatten the processed features
         x = x.flatten(1)
         return self.fc(x)
-    
-    def training_step(self, batch, batch_idx):
-        # return super().training_step(*args, **kwargs)
-        x, y = batch
-        outputs = self(x)
-
-        loss = self.loss_fn(outputs, y)
-        train_accuracy = self.accuracy(outputs, y)
-
-        self.log("train_loss", loss, on_epoch=True)
-        self.log("train_accuracy", train_accuracy, on_epoch=True)
-
-        return loss
-    
-    # def test_step(self, batch, batch_idx):
-    #     x, y = batch
-    #     logits = self(x)
-
-    #     y_preds = torch.argmax(logits)
-
-    #     return y_preds.cpu().numpy(), y.cpu().numpy()
-    
-    def configure_optimizers(self):
-        return optim.AdamW(self.parameters(), lr=1e-3)
