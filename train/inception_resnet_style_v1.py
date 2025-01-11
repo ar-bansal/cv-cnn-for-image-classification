1d9ee0df263dd78022fbfd454b855102f7e8304a
Aryan Bansal <aryanbansal1710@gmail.com>
2025-01-11 11:53:00 -0800

    create a training script for InceptionResNetStyleV1

---
+++ train/inception_resnet_style_v1.py
@@ -0,0 +1,24 @@
+import os
+from ml.models import InceptionResNetStyleV1
+from ml.pipelines import run_pipeline
+
+
+if __name__ == "__main__":
+    MODEL = InceptionResNetStyleV1()
+    BATCH_SIZE = 64
+    NUM_EPOCHS = 100
+    NUM_WORKERS = os.cpu_count()
+    VAL_SIZE = 0.05
+    EXPERIMENT_NAME = "inception-resnet-style-cnn"
+    RANDOM_STATE = 0
+
+    run_pipeline(
+        MODEL, 
+        BATCH_SIZE, 
+        NUM_EPOCHS, 
+        NUM_WORKERS, 
+        VAL_SIZE, 
+        EXPERIMENT_NAME, 
+        RANDOM_STATE
+    )
+    
