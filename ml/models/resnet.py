b477b8d0572db5520cc2346fecf9dfe22b7edfc3
Aryan Bansal <aryanbansal1710@gmail.com>
2025-01-14 14:27:38 -0800

    fix ResNet-1's architecture to use ReLU AFTER the main + residual operation in the skip-connected block.

--- ml/models/resnet.py
+++ ml/models/resnet.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from .utils import Model, ConvBlock, SkipConnection
 
@@ -7,53 +8,62 @@ class ResNetStyleV1(Model):
     def __init__(self):
         super(ResNetStyleV1, self).__init__()
 
+        self.relu = nn.ReLU()
         # Input shape = (B, 3, 32, 32)
         self.conv1 = ConvBlock(3, 32, kernel_size=3, stride=1, padding=1)
         #  Output shape = (B, 32, 32, 32)
 
-        self.res1 = SkipConnection(
+        self.res2 = SkipConnection(
             nn.Sequential(
                 ConvBlock(32, 32, kernel_size=3, stride=1, padding=1), 
-                ConvBlock(32, 32, kernel_size=3, stride=1, padding=1)
+                nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
             )
         )
         # Output shape = (B, 32, 32, 32)
 
-        self.res2 = SkipConnection(
+        self.res3 = SkipConnection(
             nn.Sequential(
                 ConvBlock(32, 64, kernel_size=3, stride=2, padding=1), 
-                ConvBlock(64, 64, kernel_size=3, stride=1, padding=1)
+                nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
             ), 
             downsample=nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
         )
         # Output shape = (B, 64, 16, 16)
 
-        self.res3 = SkipConnection(
+        self.res4 = SkipConnection(
             nn.Sequential(
                 ConvBlock(64, 128, kernel_size=3, stride=2, padding=1), 
-                ConvBlock(128, 128, kernel_size=3, stride=1, padding=1)
+                nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
             ), 
             downsample=nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
         )
         # Output shape = (B, 128, 8, 8)
 
-        self.pool4 = nn.AdaptiveAvgPool2d((1, 1))
+        self.pool5 = nn.AdaptiveAvgPool2d((1, 1))
         # Output shape = (B, 128, 1, 1)
 
         self.flatten = nn.Flatten(1)
         # Output shape = (B, 128)
 
-        self.fc5 = nn.Linear(128, 10)
+        self.fc6 = nn.Linear(128, 10)
 
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.res1(x)
+
         x = self.res2(x)
+        x = self.relu(x)
+
         x = self.res3(x)
-        x = self.pool4(x)
+        x = self.relu(x)
+
+        x = self.res4(x)
+        x = self.relu(x)
+
+        x = self.pool5(x)
         x = self.flatten(x)
-        x = self.fc5(x)
+
+        x = self.fc6(x)
 
         return x
     
