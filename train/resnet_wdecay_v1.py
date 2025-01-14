dede562a7835d7c6e38f6bcc22512304acc69163
Aryan Bansal <aryanbansal1710@gmail.com>
2025-01-13 17:22:43 -0800

    create training scripts for Dropout-1 and WDecay-1

---
+++ train/resnet_wdecay_v1.py
@@ -0,0 +1,24 @@
+import os
+from ml.models import ResNetWDecayV1
+from ml.pipelines import run_pipeline
+
+
+if __name__ == "__main__":
+    MODEL = ResNetWDecayV1()
+    BATCH_SIZE = 64
+    NUM_EPOCHS = 100
+    NUM_WORKERS = os.cpu_count()
+    VAL_SIZE = 0.05
+    EXPERIMENT_NAME = "resnet-style-cnn"
+    RANDOM_STATE = 0
+
+    run_pipeline(
+        MODEL, 
+        BATCH_SIZE, 
+        NUM_EPOCHS, 
+        NUM_WORKERS, 
+        VAL_SIZE, 
+        EXPERIMENT_NAME, 
+        RANDOM_STATE
+    )
+    
