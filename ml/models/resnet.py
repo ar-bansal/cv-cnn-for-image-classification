7fdc520276749809163d6851e67bc2079141c14d
Aryan Bansal <aryanbansal1710@gmail.com>
2025-01-16 19:00:19 -0800

    create ResNet-V2 + Dropout and BN variants

--- ml/models/resnet.py
+++ ml/models/resnet.py
@@ -192,6 +192,191 @@ class ResNetDropoutV4(ResNetStyleV1):
         # Output shape = (B, 128, 8, 8)
 
 
+class ResNetV2(Model):
+    def __init__(self):
+        super().__init__()
+
+        self.relu = nn.ReLU()
+        # Input shape = (B, 3, 32, 32)
+        self.conv1 = ConvBlock(3, 16, kernel_size=3, stride=1, padding=1)
+        #  Output shape = (B, 16, 32, 32)
+
+        self.res2 = SkipConnection(
+            nn.Sequential(
+                ConvBlock(16, 16, kernel_size=3, stride=1, padding=1), 
+                nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
+            )
+        )
+        # Output shape = (B, 16, 32, 32)
+
+        self.res3 = SkipConnection(
+            nn.Sequential(
+                ConvBlock(16, 32, kernel_size=3, stride=2, padding=1), 
+                nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+            ), 
+            downsample=nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1)
+        )
+        # Output shape = (B, 32, 16, 16)
+
+        self.res4 = SkipConnection(
+            nn.Sequential(
+                ConvBlock(32, 64, kernel_size=3, stride=2, padding=1), 
+                nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+            ), 
+            downsample=nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
+        )
+        # Output shape = (B, 64, 8, 8)
+
+        self.pool5 = nn.AdaptiveAvgPool2d((1, 1))
+        # Output shape = (B, 64, 1, 1)
+
+        self.flatten = nn.Flatten(1)
+        # Output shape = (B, 64)
+
+        self.fc6 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+
+        x = self.res2(x)
+        x = self.relu(x)
+
+        x = self.res3(x)
+        x = self.relu(x)
+
+        x = self.res4(x)
+        x = self.relu(x)
+
+        x = self.pool5(x)
+        x = self.flatten(x)
+
+        x = self.fc6(x)
+
+        return x    
+
+
+class ResNetV2_DropoutV1(ResNetV2):
+    """
+    Use dropout after the GAP layer
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.conv1(x)
+
+        x = self.res2(x)
+        x = self.relu(x)
+
+        x = self.res3(x)
+        x = self.relu(x)
+
+        x = self.res4(x)
+        x = self.relu(x)
+
+        x = self.pool5(x)
+        x = self.flatten(x)
+        x = self.dropout(x)
+
+        x = self.fc6(x)
+
+        return x
+
+
+class ResNetV2_DropoutV2(ResNetV2):
+    """
+    Use dropout2d before the GAP layer
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.dropout2d = nn.Dropout2d(0.5)
+
+    def forward(self, x):
+        x = self.conv1(x)
+
+        x = self.res2(x)
+        x = self.relu(x)
+
+        x = self.res3(x)
+        x = self.relu(x)
+
+        x = self.res4(x)
+        x = self.relu(x)
+
+        x = self.dropout2d(x)
+        x = self.pool5(x)
+        x = self.flatten(x)
+
+        x = self.fc6(x)
+
+        return x
+    
+
+class ResNetV2_BNV1(ResNetV2):
+    def __init__(self):
+        super().__init__()
+
+        self.relu = nn.ReLU()
+        # Input shape = (B, 3, 32, 32)
+        self.conv1 = ConvBlock(3, 16, kernel_size=3, stride=1, padding=1)
+        #  Output shape = (B, 16, 32, 32)
+
+        self.res2 = SkipConnection(
+            nn.Sequential(
+                nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(16), 
+                nn.ReLU(), 
+                nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1), 
+                nn.BatchNorm2d(16), 
+            )
+        )
+        # Output shape = (B, 16, 32, 32)
+
+        self.res3 = SkipConnection(
+            nn.Sequential(
+                nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1), 
+                nn.BatchNorm2d(32), 
+                nn.ReLU(), 
+                nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1), 
+                nn.BatchNorm2d(32), 
+            ), 
+            downsample=nn.Sequential(
+                nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1), 
+                nn.BatchNorm2d(32), 
+            )
+        )
+        # Output shape = (B, 32, 16, 16)
+
+        self.res4 = SkipConnection(
+            nn.Sequential(
+                nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1), 
+                nn.BatchNorm2d(64), 
+                nn.ReLU(), 
+                nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1), 
+                nn.BatchNorm2d(64), 
+            ), 
+            downsample=nn.Sequential(
+                nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1), 
+                nn.BatchNorm2d(64), 
+            )
+        )
+        # Output shape = (B, 64, 8, 8)
+
+        self.pool5 = nn.AdaptiveAvgPool2d((1, 1))
+        # Output shape = (B, 64, 1, 1)
+
+        self.flatten = nn.Flatten(1)
+        # Output shape = (B, 64)
+
+        self.fc6 = nn.Linear(64, 10)
+
+    
+
 class ResNetWDecayV1(ResNetStyleV1):
     def __init__(self):
         super(ResNetWDecayV1, self).__init__()
