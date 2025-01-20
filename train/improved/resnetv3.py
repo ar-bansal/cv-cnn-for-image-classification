e4882c9690eb92fd0907a313498bb1c340a86bd5
Aryan Bansal <aryanbansal1710@gmail.com>
2025-01-19 17:38:04 -0800

    increase epochs to 200

--- train/improved/resnetv3.py
+++ train/improved/resnetv3.py
@@ -6,7 +6,7 @@ from ml.pipelines import run_pipeline
 if __name__ == "__main__":
     MODEL = ResNetV3()
     BATCH_SIZE = 64
-    NUM_EPOCHS = 100
+    NUM_EPOCHS = 200
     NUM_WORKERS = os.cpu_count()
     VAL_SIZE = 0.05
     EXPERIMENT_NAME = "resnet-style-cnn"
