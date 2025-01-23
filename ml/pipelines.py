a34143df30ec36bd1a8159e0c60ecf0c5b4a5acb
Aryan Bansal <aryanbansal1710@gmail.com>
2025-01-22 19:53:10 -0800

    use pytorch_lightning's data module

--- ml/pipelines.py
+++ ml/pipelines.py
@@ -1,17 +1,8 @@
-import os
 import numpy as np
 import pandas as pd
-import mlflow
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
-import torch
-from torch.utils.data import DataLoader, Subset
-from torchvision.datasets import CIFAR10
-from torchvision import transforms
 from pytorch_lightning import Trainer
-from mlops.ml_logging import get_tracking_uri
 from mlops.ml_logging.loggers import PyTorchLogger
-
 
 
 __all__ = ["run_pipeline"]
@@ -26,30 +17,27 @@ def create_trainer(n_epochs: int=None) -> Trainer:
     return trainer
 
 
-def train(model, train_dataloader, val_dataloader, n_epochs):
+def train(model, datamodule, n_epochs):
     trainer = create_trainer(n_epochs)
     trainer.fit(
         model=model, 
-        train_dataloaders=train_dataloader, 
-        val_dataloaders=val_dataloader
+        datamodule=datamodule
         )
 
     return trainer.model
 
 
-def evaluate(model, test_dataloader):
-    model.eval()
-    all_predictions = []
-    all_targets = []
+def evaluate(model, datamodule):
+    trainer = create_trainer()
 
-    with torch.no_grad():
-        for x, targets in test_dataloader:
-            logits = model(x)
-            y_pred = torch.argmax(logits, 1)
-            all_predictions.append(y_pred.cpu().numpy())
-            all_targets.append(targets.cpu().numpy())
+    pred_labels = trainer.predict(
+        model=model, 
+        datamodule=datamodule
+    )
 
-    return np.concatenate(all_predictions), np.concatenate(all_targets)
+    true_labels = datamodule.test_dataset.targets
+
+    return np.concatenate(pred_labels).reshape(-1, 1), true_labels
 
 
 def get_metrics(predictions, targets, class_labels=None):
@@ -84,71 +72,18 @@ def get_metrics(predictions, targets, class_labels=None):
 
 
 @PyTorchLogger(save_graph=True).log
-def train_and_evaluate(model, n_epochs, train_loader, val_loader, test_loader, class_labels, experiment_name, input_shape):
-    model = train(model, train_loader, val_loader, n_epochs)
+def train_and_evaluate(model, datamodule, n_epochs, class_labels, experiment_name, input_shape):
+    model = train(model, datamodule, n_epochs)
 
-    preds, targets = evaluate(model, test_loader)
+    preds, targets = evaluate(model, datamodule)
 
     metrics = get_metrics(preds, targets, class_labels=class_labels)
 
     return model, metrics
 
 
-def run_pipeline(model, batch_size, num_epochs, num_workers, val_size, experiment_name, random_state):
-    PIN_MEMORY = True if torch.cuda.is_available() else False
-    DATA_DIR = os.path.join(os.getcwd(), "data")
-    TRAIN_DATA_DIR = os.path.join(DATA_DIR, "train")
-    TEST_DATA_DIR = os.path.join(DATA_DIR, "test")
-    INPUT_SHAPE = (batch_size, 3, 32, 32)
-
-    tracking_server_uri = get_tracking_uri()
-    mlflow.set_tracking_uri(tracking_server_uri)
-
-    transform = transforms.Compose([
-        transforms.ToTensor(), 
-        transforms.Normalize(
-            mean=[0.485, 0.456, 0.406], 
-            std=[0.229, 0.224, 0.225]
-        )
-    ])
-
-    dataset = CIFAR10(root=TRAIN_DATA_DIR, train=True, transform=transform, download=True)
-    targets = dataset.targets
-
-    # Split the dataset into train and validation sets. 
-    train_indices, val_indices = train_test_split(
-        range(len(targets)), 
-        test_size=val_size, 
-        stratify=targets, 
-        random_state=random_state
-    )
-
-    train_dataset = Subset(dataset, train_indices)
-    val_dataset = Subset(dataset, val_indices)
-    test_dataset = CIFAR10(root=TEST_DATA_DIR, train=False, transform=transform, download=True)
-
-    train_loader = DataLoader(
-        train_dataset, 
-        batch_size=batch_size, 
-        shuffle=True, 
-        num_workers=num_workers, 
-        pin_memory=PIN_MEMORY
-
-    )
-    val_loader = DataLoader(
-        val_dataset, 
-        batch_size=batch_size, 
-        shuffle=False, 
-        num_workers=num_workers, 
-        pin_memory=PIN_MEMORY
-    )
-    test_loader = DataLoader(
-        test_dataset, 
-        batch_size=batch_size, 
-        shuffle=False,  
-        num_workers=num_workers, 
-        pin_memory=PIN_MEMORY
-    )
+def run_pipeline(model, datamodule, num_epochs, experiment_name):
+    INPUT_SHAPE = (1, 3, 32, 32)
 
     class_labels = [
         "airplane", "automobile", "bird", "cat", "deer", 
@@ -157,10 +92,8 @@ def run_pipeline(model, batch_size, num_epochs, num_workers, val_size, experimen
    
     train_and_evaluate(
         model, 
+        datamodule, 
         num_epochs, 
-        train_loader, 
-        val_loader, 
-        test_loader, 
         class_labels, 
         experiment_name=experiment_name, 
         input_shape=INPUT_SHAPE
