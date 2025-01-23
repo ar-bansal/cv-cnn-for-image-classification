9dc2fb65cb1d5aafbd008f0f5fd0d630a1a7d679
Aryan Bansal <aryanbansal1710@gmail.com>
2025-01-22 19:54:13 -0800

    use data modules for testing

--- scripts/test_model_setup.py
+++ scripts/test_model_setup.py
@@ -1,94 +1,45 @@
 import requests
 import mlflow
-from sklearn.model_selection import train_test_split
-import torch
-from torch.utils.data import DataLoader, Subset
-from torchvision.datasets import CIFAR10
-from torchvision import transforms
-from ml.models import ResNetV3
-from ml.pipelines import train_and_evaluate
+import time
 from mlops.server.operations import Server
+from ml.models import ResNetV3_WInit
+from ml.pipelines import run_pipeline
+from ml.datamodules import CIFAR10DataModule
 
 
 def main():
+    # Start Mlflow server 
+    # infra_server = Server()
+    # infra_server.start()
+
+    # time.sleep(15)
+
     # MLFLOW_TRACKING_URI = "http://localhost:5001/"
+
     # try:
     #     requests.get(MLFLOW_TRACKING_URI)
     # except:
     #     raise requests.exceptions.ConnectionError("Unable to reach MLOps.")
     # mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
-    MODEL = ResNetV3()
-    BATCH_SIZE = 8
-    RANDOM_STATE = 0
-    NUM_EPOCHS = 1
-    NUM_WORKERS = 2
-    NUM_SAMPLES = 100
-    PIN_MEMORY = True if torch.cuda.is_available() else False
-
-    transform = transforms.Compose([
-        transforms.ToTensor(), 
-        transforms.Normalize(
-            mean=[0.485, 0.456, 0.406], 
-            std=[0.229, 0.224, 0.225]
-        )
-    ])
-
-    dataset = CIFAR10(root="./data/train", train=True, transform=transform, download=True)
-    targets = dataset.targets
-
-    # Split the dataset into train and validation sets. 
-    train_indices, val_indices = train_test_split(
-        range(len(targets)), 
-        test_size=0.2, 
-        stratify=targets, 
-        random_state=RANDOM_STATE
-    )
-
-    train_dataset = Subset(dataset, train_indices[:NUM_SAMPLES])
-    val_dataset = Subset(dataset, val_indices[:NUM_SAMPLES])
-    test_dataset = Subset(
-        CIFAR10(root="./data/test", train=False, transform=transform, download=True), 
-        range(NUM_SAMPLES)
-    )
-
-    train_loader = DataLoader(
-        train_dataset, 
+    
+    MODEL = ResNetV3_WInit()
+    VAL_SIZE = 0.05
+    BATCH_SIZE = 256
+    NUM_EPOCHS = 2
+    EXPERIMENT_NAME = "resnet-style-cnn"
+    
+    DATAMODULE = CIFAR10DataModule(
+        val_size=VAL_SIZE, 
         batch_size=BATCH_SIZE, 
-        shuffle=True, 
-        num_workers=NUM_WORKERS, 
-        pin_memory=PIN_MEMORY
-
-    )
-    val_loader = DataLoader(
-        val_dataset, 
-        batch_size=BATCH_SIZE, 
-        shuffle=False, 
-        num_workers=NUM_WORKERS, 
-        pin_memory=PIN_MEMORY
-    )
-    test_loader = DataLoader(
-        test_dataset, 
-        batch_size=BATCH_SIZE, 
-        shuffle=False,  
-        num_workers=NUM_WORKERS, 
-        pin_memory=PIN_MEMORY
+        transform=None
     )
 
-    class_labels = [
-        "airplane", "automobile", "bird", "cat", "deer", 
-        "dog", "frog", "horse", "ship", "truck"
-    ]
-   
-    train_and_evaluate(
-        MODEL, 
-        NUM_EPOCHS, 
-        train_loader, 
-        val_loader, 
-        test_loader, 
-        class_labels, 
-        experiment_name="inception-style-cnn", 
-        input_shape=(BATCH_SIZE, 3, 32, 32)
+    run_pipeline(
+        model=MODEL, 
+        datamodule=DATAMODULE, 
+        num_epochs=NUM_EPOCHS,
+        experiment_name=EXPERIMENT_NAME, 
     )
 
 
