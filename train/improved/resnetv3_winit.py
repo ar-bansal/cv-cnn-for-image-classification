b7ae9e5839521d06810be0ded369db4ddc97f6a0
Aryan Bansal <aryanbansal1710@gmail.com>
2025-01-22 19:55:20 -0800

    use data modules

--- train/improved/resnetv3_winit.py
+++ train/improved/resnetv3_winit.py
@@ -1,24 +1,29 @@
-import os
+import mlflow
+from mlops.ml_logging import get_tracking_uri
 from ml.models import ResNetV3_WInit
 from ml.pipelines import run_pipeline
+from ml.datamodules import CIFAR10DataModule
 
 
 if __name__ == "__main__":
     MODEL = ResNetV3_WInit()
+    VAL_SIZE = 0.05
     BATCH_SIZE = 64
     NUM_EPOCHS = 200
-    NUM_WORKERS = os.cpu_count()
-    VAL_SIZE = 0.05
     EXPERIMENT_NAME = "resnet-style-cnn"
-    RANDOM_STATE = 0
+    
+    DATAMODULE = CIFAR10DataModule(
+        val_size=VAL_SIZE, 
+        batch_size=BATCH_SIZE, 
+    )
+
+    tracking_server_uri = get_tracking_uri()
+    mlflow.set_tracking_uri(tracking_server_uri)
 
     run_pipeline(
-        MODEL, 
-        BATCH_SIZE, 
-        NUM_EPOCHS, 
-        NUM_WORKERS, 
-        VAL_SIZE, 
-        EXPERIMENT_NAME, 
-        RANDOM_STATE
+        model=MODEL, 
+        datamodule=DATAMODULE, 
+        num_epochs=NUM_EPOCHS,
+        experiment_name=EXPERIMENT_NAME, 
     )
     
